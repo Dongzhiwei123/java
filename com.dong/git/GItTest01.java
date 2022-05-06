dcb0e443bd335b36bb8bc39159bb36f20402ba37
Dongzhiwei123 <81545553+Dongzhiwei123@users.noreply.github.com>
2022-05-06 14:03:26 +0800

    git 添加用于测试

--- com.dong/git/GItTest01.java
+++ com.dong/git/GItTest01.java
@@ -4,5 +4,6 @@ public class GItTest01 {
     public static void main(String[] args) {
         System.out.println("mseter01 ");
         System.out.println("hotfix02");
+        System.out.println("github 直接添加");
     }
 }
